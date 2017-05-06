b09b674cb92d41324a5dc1282fe60d295e819f82
Mayank Tanwa <mayank.tanwar@sjsu.edu>
2017-05-06 01:20:43 -0700

    completed passenger

--- src/main/java/com/airplane/passenger/PassengerController.java
+++ src/main/java/com/airplane/passenger/PassengerController.java
@@ -1,10 +1,17 @@
 package com.airplane.passenger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //import org.hibernate.annotations.common.util.impl.Log_.logger;
 
 //import java.sql.SQLIntegrityConstraintViolationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +22,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class PassengerController {
 	@Autowired
 	private PassengerService passengerService;
+	
+	/** 
+	 * CREATE PASSENGER - /passenger - METHOD - POST
+	 * @author mayanktanwar
+	 * @param firstname
+	 * @param lastname
+	 * @param age
+	 * @param gender
+	 * @param phone
+	 * @return Passenger
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(method=RequestMethod.POST,value="/passenger") 
-	public void createPassenger( @RequestBody Passenger passenger) 
+	public ResponseEntity createPassenger( @RequestBody Passenger passenger) 
 	{		
 	try{
-		passengerService.createPassenger(passenger);
+		return passengerService.createPassenger(passenger);
 	}
 	catch(Exception ex){
 		
-		System.out.println(ex.getMessage());
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity(getErrorResponse("400",ex.getMessage()), responseHeaders, HttpStatus.BAD_REQUEST);
 	}
 		
 	}
+		/**
+	 * Builds a hashmap for bad requests.
+	 * @param errorcode
+	 * @param error
+	 * @return HashMap<String, String>
+	 */
+	@SuppressWarnings("rawtypes")
+	public HashMap<String, Map> getErrorResponse(String errorcode, String error) {
+		HashMap<String, String> errorMap = new HashMap<String, String>();
+		errorMap.put("code", errorcode);
+		errorMap.put("msg", error);
+		HashMap<String, Map> errorResponse = new HashMap<String, Map>();
+		errorResponse.put("Badrequest", errorMap);
+		return errorResponse;
+	}
+	
 }
--- src/main/java/com/airplane/passenger/PassengerService.java
+++ src/main/java/com/airplane/passenger/PassengerService.java
@@ -1,20 +1,26 @@
 package com.airplane.passenger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+@SuppressWarnings({ "rawtypes", "unchecked" })
 @Service
 public class PassengerService {
 	@Autowired
 	private PassengerRepository passengerRepository;
-	public void createPassenger(Passenger passenger)
+	public ResponseEntity createPassenger(Passenger passenger)
 	{
 		
-		//Passenger passenger = new Passenger(firstname, lastname, age, gender, phone);
+		//Passenger passenger = new Passenger();
 		
 		passengerRepository.save(passenger);
-		System.out.println(passenger.getId());
-		
-		
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity(passenger, responseHeaders, HttpStatus.OK);
+		//System.out.println(passenger.getId());
 	}
 }
